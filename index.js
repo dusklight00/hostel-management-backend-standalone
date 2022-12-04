190c5bb9a693d54f9638e4ef5ed0e47b38d64455
Dusk Light <r.rahul.developer@gmail.com>
2022-12-04 13:53:29 +0530

    protected endpoint from null entires

--- index.js
+++ index.js
@@ -10,9 +10,28 @@ const {
   findStudent,
   setSessionID,
   resetSessionID,
-  findStudentBySessionID,
 } = require("./database");
 const { generateUUID } = require("./utils");
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -25,6 +44,22 @@ app.get("/register_new_student", async (req, res) => {
   const name = req.query.name;
   const usnID = req.query.usnID;
   const password = req.query.password;
+
+  if (name == null || usnID == null || password == null) {
+    res.send({
+      status: "field_missing",
+      message: "name, usnID or password is misisng.",
+    });
+    return false;
+  }
+
+  if (name == "" || usnID == "" || password == "") {
+    res.send({
+      status: "field_missing",
+      message: "Length of name or usnID or password is greater than 0",
+    });
+    return false;
+  }
 
   const isStudentAlreadyExist = await isStudentExist(usnID);
   if (isStudentAlreadyExist) {
@@ -50,6 +85,15 @@ app.get("/register_new_student", async (req, res) => {
 
 app.get("/mark_present", async (req, res) => {
   const usnID = req.query.usnID;
+
+  if (usnID == null) {
+    res.send({
+      status: "field_missing",
+      message: "usnID is missing",
+    });
+    return false;
+  }
+
   const isAlreadyPresent = await isStudentPresentToday(usnID);
   if (isAlreadyPresent) {
     res.send({
@@ -87,23 +131,56 @@ app.get("/reset_outing_for_all", async (req, res) => {
 
 app.get("/get_student_detail", async (req, res) => {
   const usnID = req.query.usnID;
+
+  if (usnID == null) {
+    res.send({
+      status: "field_missing",
+      message: "One of the fields are missing",
+    });
+    return false;
+  }
+
+  if (usnID == "") {
+    res.send({
+      status: "field_missing",
+      message: "Length of usnID is greater than 0",
+    });
+    return false;
+  }
+
   const student = await findStudent(usnID);
   res.send(student);
-});
-
-app.get("/get_usn_id_from_session_id", async (req, res) => {
-  const sessionID = req.query.sessionID;
-  const student = await findStudentBySessionID(sessionID);
-  const usnID = student.usnID;
-  res.send({
-    usnID: usnID,
-  });
 });
 
 app.get("/login", async (req, res) => {
   const usnID = req.query.usnID;
   const password = req.query.password;
+
+  if (usnID == null || password == null) {
+    res.send({
+      status: "field_missing",
+      message: "usnID or password is missing.",
+    });
+    return false;
+  }
+
+  if (usnID == "" || password == "") {
+    res.send({
+      status: "field_missing",
+      message: "Length of usnID or password is greater than 0",
+    });
+    return false;
+  }
+
   const student = await findStudent(usnID);
+  if (student == null) {
+    res.send({
+      status: "user_not_found",
+      message: "No user exists with this usnID",
+    });
+    return false;
+  }
+
   const actualPassword = student.password;
   // if (student.sessionID !== null) {
   //   res.send({
@@ -131,6 +208,14 @@ app.get("/login", async (req, res) => {
 app.get("/logout", async (req, res) => {
   const usnID = req.query.usnID;
   const sessionID = req.query.sessionID;
+  if (usnID == null || sessionID == null) {
+    res.send({
+      status: "field_missing",
+      message: "usnID or sessionID is missing.",
+    });
+    return false;
+  }
+
   const student = await findStudent(usnID);
   const actualSessionId = student.sessionID;
   if (sessionID != actualSessionId) {
